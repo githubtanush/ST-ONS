df885f740c0cc8e7606aa62173e1199636d3c897
Tanush Arora <tanush935@gmail.com>
2025-07-14 10:54:37 +0530

    fifth commit

--- binarysearch/lower_bound/lowerbound.cpp
+++ binarysearch/lower_bound/lowerbound.cpp
@@ -11,7 +11,7 @@ int binarysearch(vector<int>& arr,int target){
             ans = mid;
             e = mid - 1;
         }
-        else if(arr[mid] < target) e = mid - 1;
+        else if(target < arr[mid]) e = mid - 1;
         else s = mid + 1;
     }
     return ans;
--- binarysearch/upper_bound/upperbound.cpp
+++ binarysearch/upper_bound/upperbound.cpp
@@ -9,9 +9,9 @@ int binarysearch(vector<int>& arr,int target){
         int mid = s + ( e - s )/2;
         if(arr[mid] == target){
             ans = mid;
-            e = mid - 1;
+            s = mid + 1;
         }
-        else if(arr[mid] < target) e = mid - 1;
+        else if(arr[mid] > target) e = mid - 1;
         else s = mid + 1;
     }
     return ans;
