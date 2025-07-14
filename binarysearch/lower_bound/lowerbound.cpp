82b3d9d04a6dc203345e95e47c44432a72ca2526
Tanush Arora <tanush935@gmail.com>
2025-07-14 10:38:31 +0530

    fourth commit

--- binarysearch/lower_bound/lowerbound.cpp
+++ binarysearch/lower_bound/lowerbound.cpp
@@ -3,14 +3,18 @@ using namespace std;
 int binarysearch(vector<int>& arr,int target){
     int n = arr.size();
     int s = 0;
+    int ans = -1;
     int e = n - 1;
     while( s <= e ){
         int mid = s + ( e - s )/2;
-        if(arr[mid] == target) return mid;
+        if(arr[mid] == target){
+            ans = mid;
+            e = mid - 1;
+        }
         else if(arr[mid] < target) e = mid - 1;
         else s = mid + 1;
     }
-    return -1;
+    return ans;
 }
 int main(){
     int n;
